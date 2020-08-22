106074b113d9bca3f2e945a21aea001f833877fb
jlxc106 <jaylim106@gmail.com>
2020-08-22 02:14:24 -0700

    pre-eject

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "./styledItems/button";
+
+const color = "green";
 
 function App() {
+  const [toBe, setToBe] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <Button
+          onClick={() => {
+            console.log("setToBe iz true");
+            setToBe(true);
+          }}
+        >
+          Set To Be
+        </Button>
+        <p
+          css={css`
+            padding: 32px;
+            background-color: hotpink;
+            font-size: 24px;
+            border-radius: 4px;
+            &:hover {
+              color: ${toBe ? color : "black"};
+            }
+          `}
+        >
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
